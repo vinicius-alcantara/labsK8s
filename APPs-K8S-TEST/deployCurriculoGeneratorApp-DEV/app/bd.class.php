0ec3edaab7b403f163a3972961919674a114739c
vinicius-alcantara <vinicius.redes2011@gmail.com>
2021-03-16 16:33:29 -0400

    update services node port - APPs k8s

--- APPs-K8S-TEST/deployCurriculoGeneratorApp-DEV/app/bd.class.php
+++ APPs-K8S-TEST/deployCurriculoGeneratorApp-DEV/app/bd.class.php
@@ -2,9 +2,9 @@
 
 class bd {
     private $host = "10.1.203.25";
-    private $user = "curriculo";
+    private $user = "curriculo-dev";
     private $password = "h4ck3rSA@181915";
-    private $database = "curriculo";
+    private $database = "curriculo_dev";
 
     public function conn_db(){
         $link = mysqli_connect($this->host, $this->user, $this->password, $this->database) or die("Erro ao conectar ao banco de dados: " . mysqli_error());
